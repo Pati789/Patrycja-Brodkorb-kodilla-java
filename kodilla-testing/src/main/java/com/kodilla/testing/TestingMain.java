a8c76064a21f711e89db12f65e4e2af7b95be200
Patrycja Brodkorb <https://github.com/Pati789/Patrycja-Brodkorb-kodilla-java.git>
2022-05-25 18:05:05 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculaor.Calculator;
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie dxo testowania oprogramowania");
+Calculator calculator = new Calculator();
+        int result1 = Calculator.add(5, 5);
+        if (result1==10) {
+            System.out.println("test OK");
+        } else {
+    System.out.println("Error!");
+}
+
+        int result2 = Calculator.subtract(2, 7);
+        if (result2==5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
+
--- kodilla-testing/src/main/java/com/kodilla/testing/calculaor/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculaor/Calculator.java
@@ -1,2 +1,13 @@
-package com.kodilla.testing.calculaor;public class Calculator {
-}
+package com.kodilla.testing.calculaor;
+
+ public class Calculator {
+
+     public static int add(int a, int b) {
+         return a + b;
+         }
+
+     public static int subtract(int a, int b) {
+         return b - a;
+     }
+ }
+
