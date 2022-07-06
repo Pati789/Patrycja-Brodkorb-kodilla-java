a056212c36ba3809b56e3169617e1074798005f6
Patrycja Brodkorb <https://github.com/Pati789/Patrycja-Brodkorb-kodilla-java.git>
2022-07-06 20:28:50 +0200

    8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,7 +1,11 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 public class FileReader {
 
     public void readFile() {
@@ -9,5 +13,12 @@ public class FileReader {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
 
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+            fileLines.forEach(System.out::println);
+        } catch  (IOException e){
+            System.out.println("Oh no! Something went wrong ! Error: " +e);
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
     }
 }
