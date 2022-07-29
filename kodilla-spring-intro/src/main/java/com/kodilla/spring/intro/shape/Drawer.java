9c0985df8ecfb9120357e45d2a656213a512b845
Patrycja Brodkorb <https://github.com/Pati789/Patrycja-Brodkorb-kodilla-java.git>
2022-07-29 11:40:26 +0200

    10

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,15 +1,12 @@
 package com.kodilla.spring.intro.shape;
 
 public class Drawer {
-
-    public void doDrawing() {
-
-        Figure figure;
-
-        figure = new Circle();
-        figure.draw();
-
-        figure = new Triangle();
-        figure.draw();
+    final Shape shape;
+    public Drawer(final Shape shape) {
+        this.shape = shape;
     }
+        public String doDrawing() {
+        return shape.draw();
+}
+
 }
