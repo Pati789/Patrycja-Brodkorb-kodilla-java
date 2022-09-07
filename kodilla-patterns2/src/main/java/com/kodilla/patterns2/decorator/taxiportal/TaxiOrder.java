c698c2d77f4e45703d00f7cea13aa20c69794b17
Patrycja Brodkorb <https://github.com/Pati789/Patrycja-Brodkorb-kodilla-java.git>
2022-09-07 21:27:55 +0200

    20

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/taxiportal/TaxiOrder.java
@@ -0,0 +1,12 @@
+package com.kodilla.patterns2.decorator.taxiportal;
+
+import java.math.BigDecimal;
+
+public interface TaxiOrder {
+
+    BigDecimal getCost();
+    String getDescription();
+
+
+
+}
